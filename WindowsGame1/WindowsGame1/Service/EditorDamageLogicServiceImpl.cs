1f10f234988258a72d468782e8b48bd3a148a25a
jkh13 <jkcheung13@gmail.com>
2013-03-10 17:07:49 +0000

    ss

--- WindowsGame1/WindowsGame1/Service/EditorDamageLogicServiceImpl.cs
+++ WindowsGame1/WindowsGame1/Service/EditorDamageLogicServiceImpl.cs
@@ -10,20 +10,6 @@ namespace TheGameOfForever.Service
     /// </summary>
     public class EditorDamageLogicServiceImpl : IDamageLogicService
     {
-        Random random = new Random();
-        float randomPercent = 0.5f;
-
-        public void setPercent(float percent)
-        {
-            randomPercent = percent;
-        }
-
-        public float getPecent()
-        {
-            return randomPercent;
-        }
-
-
         public Damage damageDealt(Configuration.Character.ICharacterStats attacker, Configuration.Weapon.IWeaponStats weapon, Configuration.Character.ICharacterStats victim)
         {
             throw new NotImplementedException();
