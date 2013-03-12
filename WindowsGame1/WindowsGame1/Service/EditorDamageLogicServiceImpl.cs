7b1e6ce4721d7792248e734f12d83eb13c578e2c
Ali Mohsen <ali.n.mohsen@gmail.com>
2013-03-12 20:36:24 +0000

    Testing commit

--- WindowsGame1/WindowsGame1/Service/EditorDamageLogicServiceImpl.cs
+++ WindowsGame1/WindowsGame1/Service/EditorDamageLogicServiceImpl.cs
@@ -12,6 +12,22 @@ namespace TheGameOfForever.Service
     {
         public Damage damageDealt(Configuration.Character.ICharacterStats attacker, Configuration.Weapon.IWeaponStats weapon, Configuration.Character.ICharacterStats victim)
         {
+            // damage = weaponPower * modifiers - defence
+
+            int damage;
+            int modifiers;
+            int defence;
+
+            // modifiers = critModifier * evasionModifier * luckModifier
+
+            int critModifier;
+            int evasionModifier;
+            int luckModifier;
+
+            // defence = defence + armour
+
+            defence = victim.getDefense() + victim.getArmour();
+
             throw new NotImplementedException();
         }
     }
