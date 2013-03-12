31741208dede18e3b2b1b83667c4217096519e1e
Ali Mohsen <ali.n.mohsen@gmail.com>
2013-03-12 22:16:54 +0000

    editor damage service
    Not very good code, but just wanted to have an idea of what we need to
    think about.

--- WindowsGame1/WindowsGame1/Service/EditorDamageLogicServiceImpl.cs
+++ WindowsGame1/WindowsGame1/Service/EditorDamageLogicServiceImpl.cs
@@ -10,25 +10,52 @@ namespace TheGameOfForever.Service
     /// </summary>
     public class EditorDamageLogicServiceImpl : IDamageLogicService
     {
+        Random rand = new Random();
+
         public Damage damageDealt(Configuration.Character.ICharacterStats attacker, Configuration.Weapon.IWeaponStats weapon, Configuration.Character.ICharacterStats victim)
         {
-            // damage = weaponPower * modifiers - defence
 
-            int damage;
-            int modifiers;
+            //** DAMAGE CALCULATION **//
+
+            // damagePoints = weaponPower * modifiers - defence
+
+            int damagePoints;
+            float modifiers;
             int defence;
 
             // modifiers = critModifier * evasionModifier * luckModifier
 
-            int critModifier;
-            int evasionModifier;
-            int luckModifier;
+            float critModifier = 1;
+            float evasionModifier = 1;
+            float luckModifier = 1;
+
+            // Generate random number between 0 and 1. If number is within Luck
+            // apply critical modifier to damage.
+
+            if (rand.NextDouble() <= attacker.getLuck())
+            {
+                critModifier = weapon.getCrit();
+            }
+
+            modifiers = critModifier * evasionModifier * luckModifier;
 
             // defence = defence + armour
 
             defence = victim.getDefense() + victim.getArmour();
 
-            throw new NotImplementedException();
+            damagePoints = (int)(weapon.getPower() * modifiers - defence);
+
+            //** STATUS **//
+
+            // If the random number is within the victims Evasion %, the victim
+            // will dodge.
+            if (rand.NextDouble() <= victim.getEvasion())
+            {
+                return new Damage(Status.MISS, damagePoints);
+            }
+
+            return new Damage(Status.HIT, damagePoints);
+
         }
     }
 }
