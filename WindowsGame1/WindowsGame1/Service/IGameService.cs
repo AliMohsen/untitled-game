7e381fabc6a9d1f8e9f8a02b08724730e9337883
jkh13 <jkcheung13@gmail.com>
2013-03-17 00:05:32 +0000

    woops

--- WindowsGame1/WindowsGame1/Service/IGameService.cs
+++ WindowsGame1/WindowsGame1/Service/IGameService.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TheGameOfForever.Entities;
 
 namespace TheGameOfForever.Service
 {
     public interface IGameService
     {
-        void registerEntityIfNeeded();
-        void unregisterEntity();
+        void registerEntityIfNeeded(Entity entity);
+        void unregisterEntity(Entity entity);
     }
 }
